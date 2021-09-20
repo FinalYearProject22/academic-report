2638d688a2eda8496a20f60dd60e2b56ff31a446
Goodwiller <iamrahulsingh0801@gmail.com>
2021-09-20 20:15:30 +0530

    Added Navigation Bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {App, Navbar} from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Navbar /> */}
+    <Navbar />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
