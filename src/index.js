5a5bd607248127016b5640b5b0415d80ae321c92
Goodwiller <iamrahulsingh0801@gmail.com>
2022-02-08 12:04:32 +0530

    Added Footer component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Navbar} from './components/Navbar/Navbar';
+import { Footer } from './components/Footer/Footer';
 
 
 ReactDOM.render(
   <div>
-    <Navbar />
+    <Navbar/>
+    <Footer/>
   </div>,
   document.getElementById('root')
 );
