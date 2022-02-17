5b9243b0256d9e18615db6c079960a3db36c6442
Goodwiller <iamrahulsingh0801@gmail.com>
2022-02-17 19:42:09 +0530

    Added Cards

--- src/components/AdminContent/AdminContent.js
+++ src/components/AdminContent/AdminContent.js
@@ -3,7 +3,77 @@
 function AdminContent(){
     return(
         <>
-            
+            <div className="container-fluid my-5">
+                <div className="container text-light">
+                    <div className="row gy-4">
+                       <div className="card border-0 col-lg-4 text-white">
+                            <img className="card-img rounded-3 " src="http://www.freeimageslive.com/galleries/backdrops/natural/pics/abstract_water.jpg" alt="Card"></img>
+                            <div className="card-img-overlay ">
+                                <h5 className="card-title mx-2 h1">Manage Courses</h5>
+                                <p className="card-text mx-2 my-3">
+                                    <ul>
+                                        <li className="h5">Add New Course</li>
+                                        <li className="h5">Show List of existing Courses</li>
+                                        <li className="h5">Delete Course</li>
+                                    </ul>                             
+                                 </p>
+                            </div>
+                        </div>
+                        <div className="card border-0 col-lg-4 text-white">
+                            <img className="card-img img-fluid rounded-3 " src="http://www.freeimageslive.com/galleries/backdrops/natural/pics/abstract_water.jpg" alt="Card"></img>
+                            <div className="card-img-overlay ">
+                                <h5 className="card-title mx-2 h1">Manage Batches</h5>
+                                <p className="card-text mx-2 my-3">
+                                    <ul>
+                                        <li className="h5">Add New Batch</li>
+                                        <li className="h5">Show List of existing Batches</li>
+                                        <li className="h5">Modify Batch</li>
+                                    </ul>                             
+                                 </p>
+                            </div>
+                        </div>
+                        <div className="card border-0 col-lg-4 text-white">
+                            <img className="card-img img-fluid rounded-3 " src="http://www.freeimageslive.com/galleries/backdrops/natural/pics/abstract_water.jpg" alt="Card"></img>
+                            <div className="card-img-overlay ">
+                                <h5 className="card-title mx-2 h1">Manage Students</h5>
+                                <p className="card-text mx-2 my-3">
+                                    <ul>
+                                        <li className="h5">Add New Student</li>
+                                        <li className="h5">Migrate Student</li>
+                                        <li className="h5">Remove Student</li>
+                                        <li className="h5">Modify Student Details</li>
+                                    </ul>                             
+                                 </p>
+                            </div>
+                        </div>
+                        <div className="card border-0 col-lg-4 text-white">
+                            <img className="card-img img-fluid rounded-3 " src="http://www.freeimageslive.com/galleries/backdrops/natural/pics/abstract_water.jpg" alt="Card"></img>
+                            <div className="card-img-overlay ">
+                                <h5 className="card-title mx-2 h1">Manage Courses</h5>
+                                <p className="card-text mx-2 my-3">
+                                    <ul>
+                                        <li className="h5">Add New Course</li>
+                                        <li className="h5">Show List of existing Courses</li>
+                                        <li className="h5">Delete Course</li>
+                                    </ul>                             
+                                 </p>
+                            </div>
+                        </div>
+                        <div className="card border-0 col-lg-4 text-white">
+                            <img className="card-img img-fluid rounded-3 " src="http://www.freeimageslive.com/galleries/backdrops/natural/pics/abstract_water.jpg" alt="Card"></img>
+                            <div className="card-img-overlay ">
+                                <h5 className="card-title mx-2 h1">Manage Teachers</h5>
+                                <p className="card-text mx-2 my-3">
+                                    <ul>
+                                        <li className="h5">Manage CA</li>
+                                        <li className="h5">Managa PA</li>
+                                    </ul>                             
+                                 </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
