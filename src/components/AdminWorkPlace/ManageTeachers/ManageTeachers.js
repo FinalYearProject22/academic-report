197981bd07738d35644da6e5c967c7ba9b64b298
Goodwiller <iamrahulsingh0801@gmail.com>
2022-02-18 19:02:36 +0530

    .

--- src/components/AdminWorkPlace/ManageTeachers/ManageTeachers.js
+++ src/components/AdminWorkPlace/ManageTeachers/ManageTeachers.js
@@ -1,10 +1,48 @@
-
+import { getAuth,onAuthStateChanged} from "firebase/auth";
+import { getDatabase , ref, child, get} from "firebase/database";
+import {app} from "../../../Firebase/firebase";
+import { useEffect } from 'react'
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 
 function ManageTeachers(){
+    let navigate=useNavigate();
+    const [role, setRole] = useState(undefined);
+    useEffect(() => {
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+          if (user) {
+            const uid = user.uid;
+            let db=ref(getDatabase(app));
+            get(child(db, `users/${uid}`)).then((snapshot) => {
+                if (snapshot.exists()) {
+                    setRole(snapshot.val().role);
+                    console.log(snapshot.val().role);
+                } else {
+                console.log("No data available");
+                }
+            }).catch((error) => {
+                console.error(error);
+            });
+
+          } else {
+          }
+        });
+    },
+    []);
+
+    if(role===`admin`)
     return(
         <>
-        ManageTeachers
+            Hello
+        </>
+    );
+    else if(role===`student` || role===`teacher`)
+        navigate('/',{replace:true});
+    else
+    return(
+        <>
         </>
     );
 }
