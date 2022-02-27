c0e74cbf676a0bd00366705bb8c4c2b69bdee53d
Goodwiller <iamrahulsingh0801@gmail.com>
2022-02-27 23:50:49 +0530

    Completed ManageCourses

--- src/components/AdminWorkPlace/ManageCourses/ManageCourses.js
+++ src/components/AdminWorkPlace/ManageCourses/ManageCourses.js
@@ -1,11 +1,12 @@
 import { getAuth,onAuthStateChanged} from "firebase/auth";
-import { getDatabase , ref , child, get, set,onValue, remove} from "firebase/database";
+import { getDatabase , ref , child, get, set,onValue, remove,update} from "firebase/database";
 import {app} from "../../../Firebase/firebase";
 import { useEffect } from 'react'
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { BackButton } from "../../BackButton/BackButton";
 import { showModal } from "../../MainModal/MainModal";
+
 
 
 function ManageCourses(){
@@ -155,28 +156,207 @@ function ManageCourses(){
 
 function ManageSems({courseid}){
 
-
+    let name,sems,totalsem;
 
 
     if(courseid && courseid !== "null" && courseid !== "undefined" && courseid!==""){
 
-        console.log(courseid)
         let db = getDatabase();
         const dbRef = ref(db, 'courses/active/'+courseid);
-        onValue(dbRef, (snapshot) => {
-            console.log(snapshot.val());
+        onValue(dbRef, (snapshot) => {    
+            name=snapshot.val().name;
+            sems=snapshot.val().sems;
+            totalsem=snapshot.val().totalsem;
         });
+
+
+        function returnsems(totalsem,sems){
+            let semlist=[]
+            for (let index = 1; index <= totalsem; index++) {
+                let semname=`sem`+index;
+                semlist.push(sems[semname])
+            }
+            return semlist;
+        }
+
+        function addsubject(courseid,semname,subjectcode,subjectname,subjecttype,subjectcredit){
+            let subjects=[];
+            let db=ref(getDatabase(app));
+
+
+            get(child(db, `courses/active/`+courseid+`/sems/`+semname+`/subs`)).then((snapshot) => {
+
+                    if(snapshot.val()===null){
+                        let db=(getDatabase(app));
+                            set(ref(db, `courses/active/`+courseid+`/sems/`+semname+`/subs/`+subjectcode+`/`), {
+                                name:subjectname,
+                                type:subjecttype,
+                                credits:subjectcredit
+                            });
+                            showModal(`New Subject is Added`, `${subjectname} Added`);
+                    }
+                    else{
+                        subjects=Object.keys(snapshot.val());
+                        let flag=0;
+                        subjects.forEach(element => {
+                            if(element===subjectcode)
+                                flag=1;
+                        });
+                        if(flag===0){
+                            let db=(getDatabase(app));
+                            set(ref(db, `courses/active/`+courseid+`/sems/`+semname+`/subs/`+subjectcode+`/`), {
+                                name:subjectname,
+                                type:subjecttype,
+                                credits:subjectcredit
+                            });
+                            showModal(`New Subject is Added`, `${subjectname} Added`);
+                        }
+                        else{
+                            showModal(`Subject can not be Added `, `Delete the Subject with Id: ${subjectcode} and then try`);
+                        }
+
+                    }
+            
+            }).catch((error) => {
+                console.error(error);
+                showModal(`Error Occured`, `Subject Code And Subjact Name should contain alphanumerals only`);
+            });
+        }
+
+        function deletesubject(courseid,semname,subcode){
+                const db = getDatabase();
+                remove(ref(db, 'courses/active/'+courseid+`/sems/`+semname+`/subs/`+subcode), )
+                .then(() => {
+                showModal(`Subject Deleted Succesfully`, `Subject Deleted Succesfully`);
+                })
+                .catch((error) => {
+                });
+        }
+
+        function semaccordion(semlist,name,totalsem){
+
+            let semaccordionlist=[]
+            
+            for(let index=0;index<totalsem;index++){
+                if(semlist[index].subs && semlist[index].subs !== "null" && semlist[index].subs !== "undefined")
+                    var subdata = Object.keys(semlist[index].subs).map((key) => [key, semlist[index].subs[key]]);
+                else
+                    var subdata=[];
+                semaccordionlist.push(
+                <div key={`sem`+(index+1)}className="accordion-item">
+                    <h2 className="accordion-header" id={"accheadingsem"+(index+1)}>
+                        <button className="accordion-button collapsed rounded-3 collapsed text-white fs-5 my-2" type="button" data-bs-toggle="collapse" data-bs-target={"#accbodysem"+(index+1)} aria-expanded="false" aria-controls={"accbodysem"+(index+1)}>
+                            Semester {index+1}
+                        </button>
+                    </h2>
+                    <div id={"accbodysem"+(index+1)} className="accordion-collapse collapse" aria-labelledby={"accheadingsem"+(index+1)} data-bs-parent="#semaccordion" >
+                        <div className="accordion-body">
+                            <div className="container">
+                                <div className="container fs-5 text-primary">
+                                <strong>Add New Subject</strong>
+                                </div>
+                                <div className="container">
+                                    <div className="row">
+                                        <div className="col-md-6  p-2">
+                                            <div className="input-group  ">
+                                                <input id={"subjectcodesem"+(index+1)} type="text" className="form-control" patter="[a-zA-Z0-9\s]+" placeholder="Subject Code (Alphanumerals)" aria-label="Subject Code"></input>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-6 p-2">
+                                        <div className="input-group ">
+                                            <input id={"subjectnamesem"+(index+1)} type="text" className="form-control" placeholder="Subject Name (Alphanumerals)" aria-label="Subject Name"></input>
+                                        </div>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-6  p-2">
+                                            <div className="input-group  ">
+                                                <label className="input-group-text" htmlFor="degreetype">Subject Type</label>
+                                                <select id={"subjecttypesem"+(index+1)} defaultValue={'Theory'} className="form-select" >
+                                                    <option  value="Theory">Theory</option>
+                                                    <option value="Practical">Practical</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-6 p-2">
+                                        <div className="input-group ">
+                                            <input id={"subjectcreditssem"+(index+1)} type="number" className="form-control" placeholder="Subject Credits (Numerals)" aria-label="Subject Credits"></input>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="container text-center my-3">
+                                        <button  onClick={
+                                            
+                                            ()=>{
+                                                addsubject(courseid,`sem`+ (index+1),document.getElementById("subjectcodesem"+(index+1)).value,document.getElementById("subjectnamesem"+(index+1)).value,document.getElementById("subjecttypesem"+(index+1)).value,document.getElementById("subjectcreditssem"+(index+1)).value)
+                                            }
+                                            }className="btn btn-primary btn-lg">Add Subject</button>
+                                </div>
+                            </div>
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                    <th scope="col">Subject Code</th>
+                                    <th scope="col">Subject Name</th>
+                                    <th scope="col">Subject Type</th>
+                                    <th scope="col">Subject Credits</th>
+                                    <th scope="col">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                {
+
+                                    subdata.map((item)=>(
+                                                    <tr key={item[0]} >
+                                                    <td>
+                                                        <div>
+                                                            {item[0]}
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                    <div>{item[1].name}</div>
+                                                    </td>
+                                                    <td>{item[1].type}</td>
+                                                    <td>{item[1].credits}</td>
+                                                    <td>
+                                                    <button onClick={()=>{deletesubject(
+                                                        courseid,`sem`+ (index+1),item[0]
+                                                    )}} className="btn btn-sm btn-danger">
+                                                        Delete
+                                                    </button>
+                                                    </td>
+                                                    </tr>
+    
+                                            ))
+                                }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                );
+            }
+            return(
+                semaccordionlist
+            );
+
+        }
 
         return(
             
             <div className="container">
-            {courseid}
+                <div className="my-3 mx-3 h5 text-success">
+                    {name}
+                </div>
+
             <div className="container">
-                
+            <div className="accordion accordion-flush" id="semaccordion">
+                {
+                   semaccordion( returnsems(totalsem,sems),name,totalsem)
+                }
             </div>
-            <button onClick={()=>{
-                document.getElementById("courseselectorbtn").classList.remove(`disabled`);
-            }}className="btn btn-primary">Save</button>
+            </div>
             </div>
         );
     }
@@ -215,7 +395,7 @@ function ModifyCourse(){
         <>
             <div className="container" id="courseselector">
             <div className="row">
-                <div className="col-md-8 bg-dark p-2">
+                <div className="col-md-8 p-2">
                     <div className="input-group  ">
                         <label className="input-group-text" htmlFor="degreetype">Select Course</label>
                         <select defaultValue={''} className="form-select" id="coursetomodify">
@@ -227,10 +407,9 @@ function ModifyCourse(){
                         </select>
                     </div>
                 </div>
-                <div className="col-md-4 bg-info p-2">
+                <div className="col-md-4 p-2">
                     <div className="container text-center">
                         <button onClick={()=>{
-                            document.getElementById("courseselectorbtn").classList.add(`disabled`);
                             setcourseid(document.getElementById("coursetomodify").value)
                         }} id="courseselectorbtn" className="btn btn-primary btn">Modify Course</button>
                     </div>
@@ -312,7 +491,8 @@ function addCourse(){
         let sems={};
         for(let i=0; i<semesters;i++){
             sems[`sem`+(i+1)]={
-                subjects:0
+                subjects:true,
+
             };
         }
         return sems;
