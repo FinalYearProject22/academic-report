0f9e42c281b3fd85ff83e066876aa87253f88283
Goodwiller <iamrahulsingh0801@gmail.com>
2022-03-01 00:49:30 +0530

    MAnage Students almost Complete

--- src/components/AdminWorkPlace/ManageStudents/ManageStudents.js
+++ src/components/AdminWorkPlace/ManageStudents/ManageStudents.js
@@ -1,13 +1,13 @@
 import './ManageStudents.css';
 import { BackButton } from "../../BackButton/BackButton";
-import { getAuth,onAuthStateChanged,createUserWithEmailAndPassword} from "firebase/auth";
+import { getAuth,onAuthStateChanged} from "firebase/auth";
 import { getDatabase , ref, child, get,set, remove} from "firebase/database";
 import {app} from "../../../Firebase/firebase";
 import { useEffect } from 'react'
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { showModal } from '../../MainModal/MainModal';
-import { StudentContentProfile } from '../../StudentContentProfile/StudentContentProfile';
+
 
 
 
@@ -126,6 +126,27 @@ function ManageStudents(){
                         </h2>
                         <div id="StudentDetailsBody" className="accordion-collapse collapse" aria-labelledby="StudentDetailsHeader" data-bs-parent="#ManageStudentAccordion">
                             <div className="accordion-body">
+                            <form className="container">
+                                <div className="my-3 mx-3 h5 text-success">
+                                    Enter Student Roll Number
+                                </div>
+                                <div className="row">
+                                    <div className="col-md-6  p-2">
+                                        <div className="input-group  ">
+                                            <input id="studentsearchroll" type="number" className="form-control" placeholder="Student unique Rollno" aria-label="Student unique Rollno"></input>
+
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="text-center">
+                                                <button onClick={()=>{
+                                                    studentdetails(document.getElementById("studentsearchroll").value);
+                                                }} className="btn btn-primary btn-lg"> Show Details</button>
+                                        </div>                                
+                                    </div>
+                                </div>
+
+                            </form>
                             </div>
                         </div>
                     </div>
@@ -137,6 +158,72 @@ function ManageStudents(){
                         </h2>
                         <div id="ModifyStudentBody" className="accordion-collapse collapse" aria-labelledby="ModifyStudentHeader" data-bs-parent="#ManageStudentAccordion">
                             <div className="accordion-body">
+                            <form className="container">
+                                <div className="my-3 mx-3 h5 text-success">
+                                    Enter Student Roll Number
+                                </div>
+                                <div className="row">
+                                    <div className="col-md-6  p-2">
+                                        <div className="input-group  ">
+                                            <input id="studentmodifyroll" type="number" className="form-control" placeholder="Student unique Rollno" aria-label="Student unique Rollno"></input>
+
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="text-center">
+                                                <button onClick={()=>{
+                                                    Modifyform(document.getElementById("studentmodifyroll").value);
+                                                }} className="btn btn-primary btn-lg"> Show Details</button>
+                                        </div>                                
+                                    </div>
+                                </div>
+                            </form>
+                            <form className="container" id='modifycontainer'>
+                                <div className="my-3 mx-3 h5 text-success">
+                                    Modify Student Details
+                                </div>
+                                    <div className="row">
+                                        <div className="col-md-6  p-2">
+                                            <div className="input-group  ">
+                                            <label className="input-group-text" htmlFor="modifystudentemail">Student Email</label>
+                                            <input id="modifystudentemail" disabled={true} type="email" className="form-control" placeholder="Registered Student Email" aria-label="Student Email"></input>
+
+                                            </div>
+                                        </div>
+                                        <div className="col-md-6 p-2">
+                                        <div className="input-group ">
+                                        <label className="input-group-text" htmlFor="modifystudentroll">Student Roll</label>
+                                        <input id="modifystudentroll" type="number" disabled={true} className="form-control" placeholder="Roll Number" aria-label="Roll Number"></input>
+                                        </div>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-6  p-2">
+                                            <div className="input-group  ">
+                                            <label className="input-group-text" htmlFor="modifystudentname">Student Name</label>
+                                            <input id="modifystudentname" type="text" className="form-control" placeholder="Student Name" aria-label="Student Name"></input>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-6 p-2">
+                                        <div className="input-group ">
+                                                <label className="input-group-text" htmlFor="modifystudentdept">Student Department</label>
+                                                <select defaultValue={"Computer Science"} className="form-select" id="modifystudentdept">
+                                                <option value="Information Technology">Information Technology</option>
+                                                    <option value="Computer Science">Computer Science</option>
+                                                    <option value="Electrical Engineering">Electrical Engineering</option>
+                                                    <option value="Electrical & Communication Engineering">Electrical & Communication Engineering</option>
+                                                    <option value="Applied Electronics">Applied Electronics</option>
+                                                    <option value="Mechanical Engineering">Mechanical Engineering</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="container text-center mt-3">
+                                        <button onClick={()=>{
+                                            modifydetails();
+                                        }} className="btn btn-primary btn-lg">Modify</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
@@ -148,6 +235,64 @@ function ManageStudents(){
                         </h2>
                         <div id="RemoveStudentBody" className="accordion-collapse collapse" aria-labelledby="RemoveStudentHeader" data-bs-parent="#ManageStudentAccordion">
                             <div className="accordion-body">
+                            <form className="container">
+                                <div className="my-3 mx-3 h5 text-success">
+                                    Enter Student Roll Number
+                                </div>
+                                <div className="row">
+                                    <div className="col-md-6  p-2">
+                                        <div className="input-group  ">
+                                            <input id="studentdeleteroll" type="number" className="form-control" placeholder="Student unique Rollno" aria-label="Student unique Rollno"></input>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="text-center">
+                                                <button onClick={()=>{
+                                                    Deleteform(document.getElementById("studentdeleteroll").value);
+                                                }} className="btn btn-primary btn-lg"> Show Details</button>
+                                        </div>                                
+                                    </div>
+                                </div>
+                            </form>
+                            <form className="container" id='deletecontainer'>
+                                <div className="my-3 mx-3 h5 text-success">
+                                    Student Details
+                                </div>
+                                    <div className="row">
+                                        <div className="col-md-6  p-2">
+                                            <div className="input-group  ">
+                                            <label className="input-group-text" htmlFor="deletestudentemail">Student Email</label>
+                                            <input id="deletestudentemail" disabled={true} type="email" className="form-control" placeholder="Registered Student Email" aria-label="Student Email"></input>
+
+                                            </div>
+                                        </div>
+                                        <div className="col-md-6 p-2">
+                                        <div className="input-group ">
+                                        <label className="input-group-text" htmlFor="deletestudentroll">Student Roll</label>
+                                        <input id="deletestudentroll" type="number" disabled={true} className="form-control" placeholder="Roll Number" aria-label="Roll Number"></input>
+                                        </div>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-6  p-2">
+                                            <div className="input-group  ">
+                                            <label className="input-group-text" htmlFor="deletestudentname">Student Name</label>
+                                            <input id="deletestudentname" disabled={true} type="text" className="form-control" placeholder="Student Name" aria-label="Student Name"></input>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-6 p-2">
+                                        <div className="input-group ">
+                                            <label className="input-group-text" htmlFor="deletestudentdept">Student Name</label>
+                                            <input id="deletestudentdept" disabled={true} type="text" className="form-control" placeholder="Student Department" aria-label="Student Department"></input>
+                                        </div>
+                                        </div>
+                                    </div>
+                                    <div className="container text-center mt-3">
+                                        <button onClick={()=>{
+                                            Deletestudent();
+                                        }} className="btn btn-primary btn-lg">Remove</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
@@ -164,6 +309,120 @@ function ManageStudents(){
     );
 }
 
+
+function Deleteform(roll){
+    let db=ref(getDatabase(app));
+    get(child(db, `students/${roll}/`)).then((snapshot) => {
+        console.log(snapshot.val())
+        if(snapshot.val()===null){
+            showModal(`Student not found`,`No Student with roll: ${roll} exists`)
+        }
+        else{
+            get(child(db, `users/${snapshot.val().uid}/`)).then((snapshot) => {
+                console.log(snapshot.val())
+                document.getElementById("deletestudentemail").value=snapshot.val().email;
+                document.getElementById("deletestudentroll").value=snapshot.val().roll;
+                document.getElementById("deletestudentname").value=snapshot.val().name;
+                document.getElementById("deletestudentdept").value=snapshot.val().dept;
+                document.getElementById("deletecontainer").style.display = "block";
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+
+function Deletestudent(){
+    let roll=document.getElementById("deletestudentroll").value;
+    let db=ref(getDatabase(app));
+    get(child(db, `students/${roll}/`)).then((snapshot) => {
+        console.log(snapshot.val().uid)
+        let db1=(getDatabase(app));
+        remove(ref(db1,`users/${snapshot.val().uid}`));
+        remove(ref(db1,`students/${roll}`));
+        document.getElementById("deletecontainer").style.display = "none";
+        showModal(``,`Student removed Successfully`);
+
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+
+function modifydetails(){
+    let newemail=document.getElementById("modifystudentemail").value;
+    let newroll=document.getElementById("modifystudentroll").value;
+    let newname=document.getElementById("modifystudentname").value;
+    let newdept=document.getElementById("modifystudentdept").value;
+    let db=ref(getDatabase(app));
+    get(child(db, `students/${newroll}/`)).then((snapshot) => {
+        console.log(snapshot.val().uid)
+        let db1=(getDatabase(app));
+        set(ref(db1, `users/${snapshot.val().uid}/`), {
+            uid:snapshot.val().uid,
+            role:`student`,
+            name:newname,
+            dept:newdept,
+            roll:newroll,
+            email:newemail
+            
+        });
+        document.getElementById("modifycontainer").style.display = "none";
+        showModal(`Modify Student`,`Changed Student details succesfully`);
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+
+function Modifyform(roll){
+    let db=ref(getDatabase(app));
+    get(child(db, `students/${roll}/`)).then((snapshot) => {
+        console.log(snapshot.val())
+        if(snapshot.val()===null){
+            showModal(`Student not found`,`No Student with roll: ${roll} exists`)
+        }
+        else{
+            get(child(db, `users/${snapshot.val().uid}/`)).then((snapshot) => {
+                console.log(snapshot.val())
+                document.getElementById("modifystudentemail").value=snapshot.val().email;
+                document.getElementById("modifystudentroll").value=snapshot.val().roll;
+                document.getElementById("modifystudentname").value=snapshot.val().name;
+                document.getElementById("modifystudentdept").value=snapshot.val().dept;
+                document.getElementById("modifycontainer").style.display = "block";
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+
+
+function studentdetails(roll){
+    let db=ref(getDatabase(app));
+    get(child(db, `students/${roll}/`)).then((snapshot) => {
+        if(snapshot.val()===null){
+            showModal(`Student not found`,`No Student with roll: ${roll} exists`)
+        }
+        else{
+            get(child(db, `users/${snapshot.val().uid}/`)).then((snapshot) => {
+                showModal(`Student Details`,`
+                <div className='row mb-1 ms-3'><span><em>Name:</em> &nbsp;&nbsp;<span >${snapshot.val().name} </span></span></div>
+                <div className='row mb-1 ms-3'><span><em>Department:</em> &nbsp;&nbsp;<span > ${snapshot.val().dept}</span></span></div>
+                <div className='row mb-1 ms-3'><span><em>Id:</em> &nbsp;&nbsp;<span >${snapshot.val().roll} </span></span></div>
+                <div className='row mb-1 ms-3'><span><em>Email:</em> &nbsp;&nbsp;<span > ${snapshot.val().email}</span></span></div>
+                `)
+
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+}
 
 function addStudent(sroll,semail,sname,sdept){
     console.log(`${sroll} ${semail} ${sname} ${sdept}`)
@@ -202,7 +461,8 @@ function addStudent(sroll,semail,sname,sdept){
                             uid:snapshot.val().uid,
                             name:sname,
                             dept:sdept,
-                            roll:sroll
+                            roll:sroll,
+                            email:semail
 
                         });
                         set(ref(db, `students/${sroll}/`), {
